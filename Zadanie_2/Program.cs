4a2f8d67ae908c3006b0641057154715dd57a426
maximmetalnikov <maxummetalnikov@mail.ru>
2023-04-24 22:10:13 +0300

    Задание 2 выполнено

---
+++ Zadanie_2/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Введите число: ");
+int rand= int.Parse(Console.ReadLine());
+if (rand>=100 && rand<=999)
+{
+    Function(rand);
+    void Function(int a)
+    {
+        int b=a%100;
+        int c=b/10;
+        Console.WriteLine(c);
+    }
+}
+else if(rand>999)
+{
+    FunctionTwo(rand);
+    void FunctionTwo(int a)
+    {
+        while (a>999)
+        {
+            a=a/10;
+        }
+        Console.WriteLine(a%10);
+    }
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
