8fa1e8214b43b92a08b1df4f1babe90a11a67831
maximmetalnikov <maxummetalnikov@mail.ru>
2023-04-24 22:10:22 +0300

    Задание 3 выполнено

---
+++ Zadanie_3/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите число: ");
+int rand= int.Parse(Console.ReadLine());
+if (rand>=1 && rand<=5)
+{
+    Function(rand);
+}
+else if(rand>=6 && rand<=7)
+{
+    FunctionTwo(rand);
+}
+else
+{
+    Console.WriteLine("Вы ввели некорректное число");
+}
+void Function(int a)
+    {
+        Console.WriteLine("Нет");
+    }
+void FunctionTwo(int a)
+{
+    Console.WriteLine("Да");
+}
