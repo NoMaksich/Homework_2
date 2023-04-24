994d9b19dcea7963da0485abe8732e00a6dca576
maximmetalnikov <maxummetalnikov@mail.ru>
2023-04-24 22:20:49 +0300

    Выполнено первое задание

---
+++ Zadanie_001/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число: ");
+int rand= int.Parse(Console.ReadLine());
+if (rand>=100 && rand<=999)
+{
+    Function(rand);
+    void Function(int a)
+    {
+        int b=a%100;
+        int c=b/10;
+        Console.WriteLine(c);
+    }
+}
+else
+{
+    Console.WriteLine("Вы ввели не трёхзначное число");
+}
